8f1d25239ca3646598a2d4f2b82f82d02f3acf82
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-19 12:57:32 -0500

    commit tree f69cf24ee6a0427ad4573bcb0f2d74ec5ff93245

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # self.include?(".") ? self.split(".").map {|str| str.split("?")}.map {|str| str.map{|str| str.split("!")}}.flatten.delete_if {|str| str == "" || str == " "}.length : 0
+    self.include?("?" || "!" || ".") ? self.split(/[.?!]/).delete_if{|str| str == "" || str == " "}.length : 0
   end
 end
+["This, well, is a sentence.", "This is too!! And so is this, I think? Woo"]
